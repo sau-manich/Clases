b49a13b2da3926f1fb7de5b121799fe6086a4e3c
saul-manich <saulmamani73@gmail.com>
2016-09-07 23:56:51 -0400

    ClasesEjemplo

---
+++ clasesEjemplo/clasesEjemplo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesEjemplo
+{
+    class Program
+    {
+        class persona
+        {
+            private int año;
+            private string nombre;
+
+            public persona()
+            {
+                nombre = "Saul";
+            }
+
+            public persona(string nom, int age)
+            {
+                this.nombre = nom;
+                this.año = age;
+            }
+
+            public void PrintPers()
+            {
+                Console.WriteLine("{0}, {1} years old.", nombre, año);
+            }
+        }
+
+        class StringTest
+        {
+            static void Main(string[] args)
+            {
+                persona per1 = new persona("Rolf", 11);
+                persona per2 = new persona("Wolf", 10);
+                persona per3 = new persona();
+
+                Console.Write("Persona #1: ");
+                per1.PrintPers();
+                Console.Write("Persona #2: ");
+                per2.PrintPers();
+                Console.Write("Persona #3: ");
+                per3.PrintPers();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
